290113ec2203f9fad5efd9c434a99fa534affc8b
Roshan <>
2020-12-24 18:50:10 +0530

    notification

--- android/app/src/main/java/com/mzw/flutter_demo_notification_bar/MainActivity.java
+++ android/app/src/main/java/com/mzw/flutter_demo_notification_bar/MainActivity.java
@@ -1,16 +1,11 @@
 package com.mzw.flutter_demo_notification_bar;
-
-
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-
-
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -18,17 +13,23 @@ import android.app.NotificationManager;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.provider.Settings;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+//import android.support.v7.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.lang.Integer;
+import android.widget.RemoteViews;
+import android.view.View;
+import android.support.v4.media.session.MediaSessionCompat;
 
-
-public class MainActivity extends FlutterActivity {
-
+public class MainActivity extends FlutterActivity{
+  TextView title;
+ // MediaSessionCompat mediaSession;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -38,13 +39,15 @@ public class MainActivity extends FlutterActivity {
             new MethodCallHandler() {
               @Override
               public void onMethodCall(MethodCall call, Result result) {
-                // TODO
+
                 if (call.method.equals("content")) {
-                  //解析参数
                   String contentTitle = call.argument("contentTitle");
                   String contentText = call.argument("contentText");
+                    String dataTitle = call.argument("dataTitle");
+                    String dataContent = call.argument("dataContent");
+                //  title.setText(contentTitle);
 
-                  sendChatMsg(contentTitle,contentText);
+                  sendChatMsg(contentTitle,contentText,dataTitle,dataContent);
                   if (true) {
                     result.success("success");
                   } else {
@@ -59,13 +62,46 @@ public class MainActivity extends FlutterActivity {
     initNotificationManager();
   }
 
+    private void removeFirebaseOrigianlNotificaitons() {
 
-  // =========================== 通知 =============================
-  // 创建两个通知渠道  8.0新特性，不允许使用 NotificationCompat.Builder
+        //check notificationManager is available
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager == null )
+            return;
+
+        //check api level for getActiveNotifications()
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            //if your Build version is less than android 6.0
+            //we can remove all notifications instead.
+            //notificationManager.cancelAll();
+            return;
+        }
+
+        //check there are notifications
+        StatusBarNotification[] activeNotifications =
+                notificationManager.getActiveNotifications();
+        if (activeNotifications == null)
+            return;
+
+        //remove all notification created by library(super.handleIntent(intent))
+        for (StatusBarNotification tmp : activeNotifications) {
+            Log.d("FCM StatusBarNotification",
+                    "tag/id: " + tmp.getTag() + " / " + tmp.getId());
+            String tag = tmp.getTag();
+            int id = tmp.getId();
+
+            //trace the library source code, follow the rule to remove it.
+            if (tag != null && tag.contains("FCM-Notification"))
+                notificationManager.cancel(tag, id);
+        }
+    }
+
+
   private void initNotificationManager(){
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       String channelId = "chat";
-      String channelName = "聊天消息";
+      String channelName = "notification";
       int importance = NotificationManager.IMPORTANCE_HIGH;
       createNotificationChannel(channelId, channelName, importance);
     }
@@ -74,47 +110,95 @@ public class MainActivity extends FlutterActivity {
   @TargetApi(Build.VERSION_CODES.O)
   private void createNotificationChannel(String channelId, String channelName, int importance) {
     NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
-    channel.setShowBadge(true);//开启桌面角标
-    channel.setBypassDnd(true);    //设置绕过免打扰模式
-    channel.canBypassDnd();       //检测是否绕过免打扰模式
-    channel.setLockscreenVisibility(Notification.VISIBILITY_SECRET);//设置在锁屏界面上显示这条通知
+    channel.setShowBadge(true);
+    channel.setBypassDnd(true);
+    channel.canBypassDnd();
+    channel.setLockscreenVisibility(Notification.VISIBILITY_SECRET);
 
-    //是否需要振动提示
+
     channel.enableVibration(true);
-    //振动模式
-    // channel.setVibrationPattern(new long[]{100, 200});
 
-    //是否需要呼吸灯提示
+
+
     channel.enableLights(true);
-    //呼吸灯颜色
-    // channel.setLightColor(color.cyan);
+
 
     NotificationManager notificationManager = (NotificationManager) getSystemService( NOTIFICATION_SERVICE);
     notificationManager.createNotificationChannel(channel);
   }
-  public void sendChatMsg(String contentTitle,String contentText) {
+  public void sendChatMsg(String contentTitle,String contentText,String dataTitle,String dataContent ) {
     NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-    //检查权限是否开启
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+    RemoteViews notificationLayout = new RemoteViews(getPackageName(), R.layout.notification_view);
+      RemoteViews expandedView = new RemoteViews(getPackageName(), R.layout.expanded_notification);
+
+      expandedView.setTextViewText(R.id.content,contentTitle);
+      expandedView.setTextViewText(R.id.content1,contentText);
+      expandedView.setTextViewText(R.id.content2,dataTitle);
+      expandedView.setTextViewText(R.id.content3,dataContent);
+
+      notificationLayout.setTextViewText(R.id.content,contentTitle);
+      notificationLayout.setTextViewText(R.id.content1,contentText);
+   // mediaSession = new MediaSessionCompat(this, "tag");
+//    Bitmap bitmap = new BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+//    String message="Hello Notification with image";
+
+//    Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.nature);
+
+
+      Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+      PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+      removeFirebaseOrigianlNotificaitons();
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       NotificationChannel channel = manager.getNotificationChannel("chat");
       if (channel.getImportance() == NotificationManager.IMPORTANCE_NONE) {
         Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
         intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
         intent.putExtra(Settings.EXTRA_CHANNEL_ID, channel.getId());
         startActivity(intent);
-        Toast.makeText(this, "请手动将通知打开", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
       }
     }
 
     Notification notification = new NotificationCompat.Builder(this, "chat")
-            .setContentTitle(contentTitle)
-            .setContentText(contentText)
-            .setWhen(System.currentTimeMillis())
+
+//            .setContentTitle(contentTitle)
+//            .setContentText(contentText)
+//            .setWhen(System.currentTimeMillis())
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+//            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.nature))
+//            .setStyle(new NotificationCompat.MediaStyle())
+            .setCustomContentView(notificationLayout)
+            .setCustomBigContentView(expandedView)
+            .setContentIntent(contentIntent)
             .setAutoCancel(true)
-            .setNumber(19)//角标显示数字
+            .setColor(0xFFff0000)
+            .setColorized(true)
+            .setNumber(19)
             .build();
+//            .setStyle(new NotificationCompat.BigPictureStyle()
+//                    .bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.nature))
+//                    .bigLargeIcon(null))
+//            .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+            /*.setShowActionsInCompactView(1)*/
+                    /*.setMediaSession(mediaSession.getSessionToken())*/
+//            .addAction(R.drawable.nature, "Dislike", null)
+
+//            .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
+//                    .setShowActionsInCompactView(1, 2, 3)
+//                    .setMediaSession(mediaSession.getSessionToken()))
+//            .setSubText("Sub Text")
+//            .setPriority(NotificationCompat.PRIORITY_HIGH)
+//            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+//            .setStyle(new NotificationCompat.BigPictureStyle()
+//                    .bigPicture(bitmap).setSummaryText(message))
+//            .setStyle(new androidx.media.app.NotificationCompat.DecoratedMediaCustomViewStyle())
+//            .setContentIntent(pendingIntent)
+
+
+//    Notification n = NotificationExtras.buildWithBackgroundColor(this, notification, 0xffff0000);
+//    manager.from(this).notify(1, n);
     manager.notify(1, notification);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: MyHome(),
   ));
@@ -13,60 +14,188 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
-  //获取到插件与原生的交互通道
-  static const mNotificationBar = const MethodChannel('notification_bar.flutter.io/notificationBar');
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+  String token = " ";
+  static const mNotificationBar =
+      const MethodChannel('notification_bar.flutter.io/notificationBar');
   TextEditingController mControllera = TextEditingController();
   TextEditingController mControllerb = TextEditingController();
 
   @override
+  void initState() {
+    getToken();
+    super.initState();
+  }
+
+  Future getToken() async {
+    /*if (Platform.isIOS) {
+      firebaseMessaging.requestNotificationPermissions(
+          IosNotificationSettings(sound: true, badge: true, alert: true));
+      firebaseMessaging.onIosSettingsRegistered
+          .listen((IosNotificationSettings settings) {
+        print("Settings registered: $settings");
+      });
+    }*/
+    firebaseMessaging.getToken().then((value) {
+      token = value.toString();
+      print("Token: $token");
+    }).catchError((onError) {
+      print("Exception: $onError");
+    });
+
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        var title = message['notification']['title'];
+        var body = message['notification']['body'];
+        var dataTitle = message['data']['title'];
+        var dataContent = message['data']['content'];
+        Map<String, String> map = {
+          "contentTitle": title,
+          "contentText": body,
+          "dataTitle": dataTitle,
+          "dataContent": dataContent,
+        };
+        await mNotificationBar.invokeMethod('content', map);
+        print("Message: $message");
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      /*(Map<String, dynamic> message) async {
+        var title = message['notification']['title'];
+        var body = message['notification']['body'];
+        var dataTitle = message['data']['title'];
+        var dataContent = message['data']['content'];
+
+        Map<String, String> map = {
+          "contentTitle": title,
+          "contentText": body,
+          "dataTitle": dataTitle,
+          "dataContent": dataContent,
+        };
+        await mNotificationBar.invokeMethod('content', map);
+        print("onBackgroundMessage: $message");
+      },*/
+      onResume: (Map<String, dynamic> message) async {
+        var title = message['notification']['title'];
+        var body = message['notification']['body'];
+        var dataTitle = message['data']['title'];
+        var dataContent = message['data']['content'];
+
+        Map<String, String> map = {
+          "contentTitle": title,
+          "contentText": body,
+          "dataTitle": dataTitle,
+          "dataContent": dataContent,
+        };
+        await mNotificationBar.invokeMethod('content', map);
+
+        print("Resume: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        var title = message['notification']['title'];
+        var body = message['notification']['body'];
+        var dataTitle = message['data']['title'];
+        var dataContent = message['data']['content'];
+
+        Map<String, String> map = {
+          "contentTitle": title,
+          "contentText": body,
+          "dataTitle": dataTitle,
+          "dataContent": dataContent,
+        };
+        await mNotificationBar.invokeMethod('content', map);
+
+        print("Launch: $message");
+      },
+    );
+  }
+
+  Future<dynamic> myBackgroundMessageHandler(
+      Map<String, dynamic> message) async {
+    if (message.containsKey('data')) {
+      var dataTitle = message['data']['title'];
+      var dataContent = message['data']['content'];
+
+      Map<String, String> map = {
+        "dataTitle": dataTitle,
+        "dataContent": dataContent,
+      };
+      await mNotificationBar.invokeMethod('content', map);
+      // Handle data message
+      final dynamic data = message['data'];
+    }
+
+    if (message.containsKey('notification')) {
+      var title = message['notification']['title'];
+      var body = message['notification']['body'];
+
+      Map<String, String> map = {
+        "contentTitle": title,
+        "contentText": body,
+      };
+      await mNotificationBar.invokeMethod('content', map);
+      // Handle notification message
+      final dynamic notification = message['notification'];
+    }
+
+    // Or do other work.
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('发送通知'),),
+        appBar: AppBar(
+          title: Text('Notification'),
+        ),
         body: Padding(
           padding: EdgeInsets.all(10),
           child: Column(
-            crossAxisAlignment : CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text('F:/VscodeProject/flutter_demo_notification_bar'),
-              SizedBox(height: 50,),
+              Text('flutter_demo_notification_bar'),
+              SizedBox(
+                height: 50,
+              ),
               TextField(
-                controller: mControllera, //控制器，控制TextField文字   同 Android View id
+                controller: mControllera,
                 decoration: new InputDecoration(
                   labelText: 'ContentTitle',
                 ),
                 autofocus: false,
               ),
-
               TextField(
-                controller: mControllerb, //控制器，控制TextField文字   同 Android View id
+                controller: mControllerb,
                 decoration: new InputDecoration(
                   labelText: 'ContentText',
                 ),
                 autofocus: false,
               ),
-              SizedBox(height: 50,),
+              SizedBox(
+                height: 50,
+              ),
               RaisedButton(
-                child: Text('发送通知'),
+                child: Text('Demo'),
                 onPressed: () async {
                   String stra = mControllera.text.trim();
                   String strb = mControllerb.text.trim();
-                  if(stra.isEmpty){
-                    stra = '收到一条聊天消息';
+                  if (stra.isEmpty) {
+                    stra = 'Empty';
                   }
-                  if(strb.isEmpty){
-                    strb = '晚上老地方不见不散！';
+                  if (strb.isEmpty) {
+                    strb = 'Empty！';
                   }
-                  Map<String, String> map = { "contentTitle": stra ,"contentText": strb};
-                  String result = await mNotificationBar.invokeMethod('content', map);
+                  Map<String, String> map = {
+                    "contentTitle": stra,
+                    "contentText": strb
+                  };
+                  String result =
+                      await mNotificationBar.invokeMethod('content', map);
                   mControllera.text = '';
                   mControllerb.text = '';
                   print(result);
-
                 },
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
